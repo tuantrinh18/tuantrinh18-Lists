b538875dbea78a77f0457c3ae4f0d19cddf11fde
tuantrinh18 <tuantrinh1803@gmail.com>
2023-02-28 00:50:25 +0000

    labdone

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,7 +30,9 @@ public class Lab {
      * @return a List<Integer> object.
      */
     public List<Integer> createList(){
-        return null;
+        
+        List<Integer> l1 = new ArrayList<>();
+        return l1;
     }
 
     /**
@@ -39,7 +42,8 @@ public class Lab {
      * @return the size of List (number of items it holds.)
      */
     public int getSize(List<Integer> list){
-        return 0;
+        
+        return list.size();
     }
 
     /**
@@ -51,7 +55,8 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addToList(List<Integer> list, int value){
-    }
+        list.add(value);
+    }   
 
     /**
      * Get a particular index of a list.
@@ -66,7 +71,7 @@ public class Lab {
      * @return the int at the location in 'list' represented by 'index'.
      */
     public int get(List<Integer> list, int index){
-        return 0;
+        return list.get(index);
     }
 
     /**
@@ -79,7 +84,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeFromList(List<Integer> list, int position){
-
+        list.remove(position);
     }
 
     /**
@@ -91,6 +96,7 @@ public class Lab {
      * @param value the new value which we would like to assign to the item at position in list
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
-    public void updateAtPosition(List<Integer> list, int position, int value){
+    public void updateAtPosition(List<Integer> list, int position, int value){ 
+        list.set(position,value);
     }
 }
